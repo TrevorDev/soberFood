da983b22080dae56209bc0cf5004444134ca9ec5
ian <idingle@uoguelph.ca>
2014-11-08 18:47:38 -0500

    standings

--- controller/college.js
+++ controller/college.js
@@ -1,25 +1,30 @@
 var User = require('./../model/user');
 var FoodItem = require('./../model/foodItem');
 var College = require('./../model/college');
+var database = require('./../lib/database');
+var sequelize = require('sequelize');
+var si = database.getSequelizeInstance();
 
 exports.getStatistics = function *() {
 	colleges = yield College.findAll()
 	list = []
 	for (var i = 0; i < colleges.length;i++) {
 		var college = colleges[i].name;
-		var eaten = yield sequelize.query('select count(*) from soberFood.Colleges' +
-	'inner join soberFood.Users on soberFood.Users.CollegeId = soberFood.Colleges.id' +
-	'inner join soberFood.FoodItems on soberFood.Users.id = soberFood.FoodItems.userId' +
-	'where soberFood.Colleges.id = ' + colleges[i].id + 'and soberFood.FoodItems.Status' +
-	' = "EATEN"', null, { raw: true }) 		
+		var eaten = yield si.query('select count(*) as num from soberFood.Colleges' +
+	' inner join soberFood.Users on soberFood.Users.CollegeId = soberFood.Colleges.id' +
+	' inner join soberFood.FoodItems on soberFood.Users.id = soberFood.FoodItems.userId' +
+	' where soberFood.Colleges.id = ' + colleges[i].id + ' and soberFood.FoodItems.Status' +
+	' = "EATEN";', null, { raw: true }) 		
 
-		var wasted = yield sequelize.query('select count(*) from soberFood.Colleges' +
-	'inner join soberFood.Users on soberFood.Users.CollegeId = soberFood.Colleges.id' +
-	'inner join soberFood.FoodItems on soberFood.Users.id = soberFood.FoodItems.userId' +
-	'where soberFood.Colleges.id = ' + colleges[i].id + 'and soberFood.FoodItems.Status' +
-	' = "EXPIRED"', null, { raw: true }) 		
+		var wasted = yield si.query('select count(*) as num from soberFood.Colleges' +
+	' inner join soberFood.Users on soberFood.Users.CollegeId = soberFood.Colleges.id' +
+	' inner join soberFood.FoodItems on soberFood.Users.id = soberFood.FoodItems.userId' +
+	' where soberFood.Colleges.id = ' + colleges[i].id + ' and soberFood.FoodItems.Status' +
+	' = "EXPIRED";', null, { raw: true }) 		
 
-		list.push({name:college, eaten: eaten, wasted: wasted})
+		console.log(eaten);
+		console.log(eaten[0].num)
+		list.push({name:college, eaten: eaten[0].num, wasted: wasted[0].num})
 	}
 
 	this.jsonResp(200, list);
